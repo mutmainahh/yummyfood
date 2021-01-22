98400b2a1e58a7674cc2b4857f6eb80072f5331a
in-stack <mutmainahh15@gmail.com>
2021-01-22 08:27:39 +0700

    yummy

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css';
+
+
+const Footer = () => (
+  <footer className="my-footer">
+     
+    <div className="text-center footer-copyright">
+      <p>Copyright &copy; 2020 YummyFood </p>
+    </div>
+  </footer>
+)
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom' 
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+    <Link className="navbar-brand" to="/">YummyFood</Link>
+  </nav>
+
+  
+)
+
+export default Navbar
